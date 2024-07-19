8be78027646b7bf04f61c0dacddd1aee3a34f796
Nicolas <nicolas.castex.pro@gmail.com>
2024-07-19 23:51:40 +0200

    init frontend, add tailwindcss, flowbite vuejs, vue router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-import './assets/main.css'
+import './assets/base.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import {createMemoryHistory, createRouter} from "vue-router";
+import HomeView from "@/components/Page/HomeView.vue";
+import ContactView from "@/components/Page/ContactView.vue";
 
-createApp(App).mount('#app')
+
+const routes = [
+    {path: '/', component: HomeView},
+    {path: '/contact', component: ContactView},
+]
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes,
+})
+
+createApp(App).use(router).mount('#app')
