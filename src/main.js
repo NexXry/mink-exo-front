80021e48ca21f8f58afc81b787b6d0cb0c9c3642
Nicolas Castex <nicolas.castex.pro@gmail.com>
2024-07-22 20:43:12 +0200

    adding Backoffice page + route and protect the route, add logout route, add authentication form and handle auth

--- src/main.js
+++ src/main.js
@@ -6,10 +6,30 @@ import {createRouter, createWebHistory} from "vue-router";
 import HomeView from "@/Page/HomeView.vue";
 import ContactView from "@/Page/ContactView.vue";
 import Vue3Toasity from 'vue3-toastify';
+import LoginView from "@/Page/LoginView.vue";
+import BackofficeView from "@/Page/BackofficeView.vue";
+import {getState, setState} from "@/Store/store.js";
 
 const routes = [
     {path: '/', component: HomeView, name: 'home'},
     {path: '/contact', component: ContactView, name: 'contact'},
+    {path: '/auth', component: LoginView, name: 'login'},
+    {
+        path: '/backoffice', component: BackofficeView, name: 'backoffice', beforeEnter: (to, from) => {
+            const token = getState().token;
+            if (!token) {
+                return '/auth'
+            }
+        }
+    },
+    {
+        path: '/logout', component: null, beforeEnter: (to, from) => {
+            setState({token: null})
+            return '/'
+        },
+        name: 'logout'
+    },
+    {path: '/:pathMatch(.*)*', redirect: '/', name: 'not-found'},
 ]
 
 const router = createRouter({
